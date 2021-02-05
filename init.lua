275842012e68c243fdac947234ade5e6ce1a53af
Koda_ <67247561+KodaTheGuy@users.noreply.github.com>
2021-02-05 17:51:27 -0400

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,10 @@
+minetest.register_node("dungeon_block:stonebrick", {
+	description = ("Dungeon Stone Brick"),
+	paramtype2 = "facedir",
+	place_param2 = 0,
+	tiles = {"default_stone_brick.png"},
+	is_ground_content = false,
+	groups = {cracky = 2, stone = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
